6146539402c7a21d028a2d3c2df9c817d9519240
Krushil Patel <>
2021-05-12 17:22:11 -0400

    Uploaded files
    This is the complete repository

---
+++ app/views.py
@@ -0,0 +1,85 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.template import loader
+from django.http import HttpResponse
+from django import template
+
+@login_required(login_url="/login/")
+def index(request):
+    
+    context = {}
+    context['segment'] = 'index'
+
+    html_template = loader.get_template( 'index.html' )
+    return HttpResponse(html_template.render(context, request))
+
+@login_required(login_url="/login/")
+def pages(request):
+    context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+        
+        load_template      = request.path.split('/')[-1]
+        context['segment'] = load_template
+        
+        html_template = loader.get_template( load_template )
+        return HttpResponse(html_template.render(context, request))
+        
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template( 'page-404.html' )
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+    
+        html_template = loader.get_template( 'page-500.html' )
+        return HttpResponse(html_template.render(context, request))
+
+def mysqllogin(request):
+    import mysql.connector
+    try:
+        ###if form.db.value="MySql"
+        host = request.POST.get('host')
+        user = request.POST.get('userid')
+        password = request.POST.get('password')
+
+        cnx = mysql.connector.connect(user='user', password='password',
+                              host='host',
+                              )
+        
+        cursor = db.cursor()
+        cursor.execute("SHOW DATABASES")
+        databases = cursor.fetchall()
+        print(databases)
+
+    except:
+
+        html_template = loader.get_template( 'page-500.html' )
+        return HttpResponse(html_template.render(context, request))
+
+def oraclelogin(request):
+    import cx_Oracle
+    import oracle.connector
+    try:
+        host = request.POST.get('host')
+        user = request.POST.get('userid')
+        password = request.POST.get('password')
+
+        cnx = oracle.connector.connect(user='user', password='password', host='host')
+
+        cursor = db.cursor()
+        cursor.execute("SHOW DATABASES")
+        databases = cursor.fetchall()
+        print(databases)
+
+    except:
+
+        html_template = loader.get_template( 'page-500.html' )
+        return HttpResponse(html_template.render(context, request))
+        
---
+++ authentication/urls.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.urls import path
+from .views import login_view, register_user, mysqllogin
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', login_view, name="login"),
+    path('register/', register_user, name="register"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path('database/', mysqllogin, name="mysqllogin")
+   path('database/', oraclelogin, name="oraclelogin")
+   
+]
